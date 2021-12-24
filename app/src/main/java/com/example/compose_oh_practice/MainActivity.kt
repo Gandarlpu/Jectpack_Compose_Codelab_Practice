042249d806ff0d62989db7b85c1490c8d2ea50d7
Chance <phs811@naver.com>
2021-12-24 16:49:47 +0900

    composeable, Preview

--- app/src/main/java/com/example/compose_oh_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_oh_practice/MainActivity.kt
@@ -10,33 +10,38 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_oh_practice.ui.theme.Compose_oh_practiceTheme
-import java.lang.reflect.Modifier
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Compose_oh_practiceTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Column(modifier = Modifier
-                        .fillMaxSize()
-                        .background(color = Color.Blue)
-                        .padding(16.dp),
-                        horizontalAlignment = Alignment.CenterHorizontally,
-                        verticalArrangement = Arrangement.SpaceBetween,
-                    ) {
-                        Text("Hello")
-                        Text("World")
-                    }
-
-                }
+                practice()
             }
         }
+    }
+}
+
+
+@Composable
+fun practice(){
+    Surface(color = MaterialTheme.colors.background) {
+        Column(modifier = Modifier
+            .background(color = Color.Blue)
+            .fillMaxSize()
+            .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceBetween,
+        ) {
+            Text("Hello")
+            Text("World")
+        }
+
     }
 }
 
@@ -45,5 +50,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     Compose_oh_practiceTheme {
+        practice()
     }
 }
